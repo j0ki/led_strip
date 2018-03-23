813d2e6bf9283b68a7e4b68ecf99e449dbe8074a
Lukas Fischer <joki0z@web.de>
2018-03-23 20:44:10 +0100

    fix lut calculation script

--- calc.rb
+++ calc.rb
@@ -11,10 +11,12 @@ rst = relative_step_times
 
 result = rst.map{|t|t*PERIOD/rst.sum}.map(&:floor)
 
-r = (1..steps).map{|k|(PI/2) - Math.acos(k.to_f/steps)}.
+r = (1-steps..steps).map{|k|(PI/2) - Math.acos(k.to_f/steps)}.
   map{|t|t*PERIOD/(PI/2)}.
   map(&:floor)
-total = r + r.map{|t|2**16 - t}.reverse
+total = r[15,15] + r[0,15].map{|t|2**16 + t}
+
+total[29] -= 1
 
 puts '{'
 puts total * ",\n"
