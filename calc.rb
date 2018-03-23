6a4fb72d8d207c0d9265f1c59babd9025b3492f1
Lukas Fischer <joki0z@web.de>
2018-03-23 21:56:11 +0100

    remove unused calculations from ruby script

--- calc.rb
+++ calc.rb
@@ -5,12 +5,6 @@ include Math
 steps = 15
 PERIOD = 2**15
 
-relative_step_times = (0...steps).map{|k|Math.sin((PI/2) * k/steps)}
-
-rst = relative_step_times
-
-result = rst.map{|t|t*PERIOD/rst.sum}.map(&:floor)
-
 r = (1-steps..steps).map{|k|(PI/2) - Math.acos(k.to_f/steps)}.
   map{|t|t*PERIOD/(PI/2)}.
   map(&:floor)
